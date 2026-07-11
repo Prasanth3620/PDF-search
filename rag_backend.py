f0a9c6672604b83f5740389d702c0278e6fae041
PRASANTH REDDI <reddiprasanth1808@gmail.com>
2026-07-11 14:51:03 +0530

    Update rag_backend.py

--- rag_backend.py
+++ rag_backend.py
@@ -1,9 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_google_genai import ( GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI,)
 
-from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_community.embeddings import HuggingFaceEmbeddings
