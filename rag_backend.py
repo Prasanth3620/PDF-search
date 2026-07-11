562a86b631664009fa25e129ff7d489280d4e189
PRASANTH REDDI <reddiprasanth1808@gmail.com>
2026-07-11 18:34:44 +0530

    Implement GeminiEmbeddings for document embedding

--- rag_backend.py
+++ rag_backend.py
@@ -1,14 +1,47 @@
+import numpy as np
+
 from google import genai
 
+from langchain_core.embeddings import Embeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+
+
+class GeminiEmbeddings(Embeddings):
+
+    def __init__(self, api_key):
+        self.client = genai.Client(api_key=api_key)
+
+    def embed_documents(self, texts):
+
+        embeddings = []
+
+        for text in texts:
+
+            response = self.client.models.embed_content(
+                model="models/gemini-embedding-001",
+                contents=text
+            )
+
+            embeddings.append(response.embeddings[0].values)
+
+        return embeddings
+
+    def embed_query(self, text):
+
+        response = self.client.models.embed_content(
+            model="models/gemini-embedding-001",
+            contents=text
+        )
+
+        return response.embeddings[0].values
 
 
 def create_vector_store(pdf_path, api_key):
 
     loader = PyPDFLoader(pdf_path)
+
     documents = loader.load()
 
     splitter = RecursiveCharacterTextSplitter(
@@ -18,13 +51,11 @@ def create_vector_store(pdf_path, api_key):
 
     chunks = splitter.split_documents(documents)
 
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
-    )
+    embedding_model = GeminiEmbeddings(api_key)
 
     vector_store = FAISS.from_documents(
         chunks,
-        embeddings
+        embedding_model
     )
 
     return vector_store
@@ -48,7 +79,7 @@ You are a helpful assistant.
 
 Answer ONLY using the provided context.
 
-If the answer is not present in the context,
+If the answer cannot be found in the context,
 reply exactly:
 
 I don't know.
@@ -60,9 +91,7 @@ Question:
 {question}
 """
 
-    client = genai.Client(
-        api_key=api_key
-    )
+    client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
