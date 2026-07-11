26f070d5d87d187f6ecb52b7fae19a28f837c655
PRASANTH REDDI <reddiprasanth1808@gmail.com>
2026-07-11 13:48:28 +0530

    Update rag_backend.py

--- rag_backend.py
+++ rag_backend.py
@@ -1,8 +1,13 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import ( GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI,)
+
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 
 def create_vector_store(pdf_path, api_key):
